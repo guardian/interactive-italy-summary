a4309d07429a08b0cefd5d208a6bb0113e4b56e7
seanclarkeguardian <sean.clarke@guardian.co.uk>
2018-02-22 15:46:10 +0000

    prepped for addition of text

--- src/render.js
+++ src/render.js
@@ -9,9 +9,20 @@ var partials = {
 	chamber: chamberTemplate
 }
 
+
+function mustachify(input) {
+    var mustached = {}
+	for (var i = 0; i < input.length; ++i)
+	{
+		mustached[input[i].id] = input[i].text;
+	}
+    return mustached;
+}
+
 export async function render() {
 	var data = (await(axios.get(config.docDataUrl))).data.sheets;
-	
+	data.mustacheCopy = mustachify(data.copy);
+	console.log(data.mustacheCopy);
 	var outputhtml = mustache.render(templateHTML,data,partials);
 	console.log(data);
     return outputhtml;
